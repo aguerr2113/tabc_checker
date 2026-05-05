c09d878c1a4afbf9bb5ad4ed5adc84f3c9b0a0f1
artG-acc <artemio.guerrero@g.austincc.edu>
2026-05-05 14:13:31 -0500

    Added a app toen text field and a button that print to the terminal when clicked

--- tabc_tool.py
+++ tabc_tool.py
@@ -16,7 +16,23 @@ header = ttk.Label(top_frame, text='TABC License Fetcher',font=('Segoe UI', 14, 
  
 header.pack(side='left')
 
+#---------------------------------------------------------------------------------
+token_var = tk.StringVar(value='')
+token_frame = ttk.Frame(window,padding=(20,6,20,0))
+token_frame.pack(fill='x')
 
+ttk.Label(token_frame,textvariable=token_var,width=40).pack(side='left',padx=8)
+
+#---------------------------------------------------------------------------------
+btn_frame = ttk.Frame(window,padding = (20,10,20,0))
+btn_frame.pack(fill='x')
+#---------------------------------------------------------------------------------
+def on_fetch_click():
+    token = token_var.get()
+    print(f'Fetch clicked! Token = {token}')
+
+fetch_btn = ttk.Button(btn_frame, text='Fetch TABC Data',command=on_fetch_click)
+fetch_btn.pack(side='left')
 
 #---------------------------------------------------------------------------------
 window.mainloop()
